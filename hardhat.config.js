3d2a217d6cfa04100e231c203ef2c87233ee8bfb
Gary Leung <master@gary.technology>
2024-05-26 22:09:32 +0800

    npm publish v2.0.4

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,88 +1,33 @@
 require("@nomicfoundation/hardhat-toolbox");
 require('@openzeppelin/hardhat-upgrades');
 require("hardhat-tracer");
-// require("hardhat-gas-reporter");
-// require('@openzeppelin/hardhat-upgrades');
-// require("@nomiclabs/hardhat-ganache");
+require('@typechain/hardhat')
+require('@nomicfoundation/hardhat-ethers')
+require('@nomicfoundation/hardhat-chai-matchers')
 require("dotenv").config();
 
-// const { ProxyAgent, setGlobalDispatcher } = require("undici");
-// const proxyAgent = new ProxyAgent('http://127.0.0.1:1081'); // change to yours
-// setGlobalDispatcher(proxyAgent);
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity:{
-    compilers:[
+  solidity: {
+    compilers: [
       {
-       version:"0.8.12",
-       settings: { optimizer: { enabled: true, runs: 200 } },
+        version: "0.8.12",
+        settings: { optimizer: { enabled: true, runs: 200 } },
       },
       {
-        version:"0.8.9",
+        version: "0.8.9",
         settings: { optimizer: { enabled: true, runs: 200 } },
-       }
-  ]
+      }
+    ]
   },
-  networks:{
-    localhost: {
-      url: "http://127.0.0.1:8545",
-    },
-    mumbai:{
-      url: "https://polygon-mumbai.blockpi.network/v1/rpc/410ce73ca9c8eed378ea7f12ccc75189e04ddc77",
+  networks: {
+    arbitrum: {
+      url: "https://arbitrum.blockpi.network/v1/rpc/public",
       accounts: [`${process.env.PK}`],
-      gasPrice:2000000000
-    },
-    goerli:{
-      url:"https://goerli.blockpi.network/v1/rpc/public",
-      accounts: [`${process.env.PK}`],
-      gasPrice:500
-    },
-    polygon:{
-      url:"https://polygon.blockpi.network/v1/rpc/2f4a5449f8f37607cf884bad80c108d3fd7d88b3",
-      // url:"https://polygon.blockpi.network/v1/rpc/public",
-      // url:"https://polygon.llamarpc.com",
-      accounts: [`${process.env.PK}`],
-      gasPrice:180000000000
-    },
-    polygon_uat:{
-      // url:"https://polygon.blockpi.network/v1/rpc/public",
-      // url:"https://polygon.llamarpc.com",
-      // url:"http://192.168.3.30:8545",
-      // url:"HTTP://127.0.0.1:7545",
-      url:"https://polygon.blockpi.network/v1/rpc/2f4a5449f8f37607cf884bad80c108d3fd7d88b3",
-      accounts: [`${process.env.PK}`],
-      gasPrice:80000000000
-    },
-    polygon_uat_dev:{
-      // url:"https://polygon.blockpi.network/v1/rpc/public",
-      // url:"https://polygon.llamarpc.com",
-      url:"https://polygon.blockpi.network/v1/rpc/2f4a5449f8f37607cf884bad80c108d3fd7d88b3",
-      accounts: [`${process.env.PK}`],
-      gasPrice:110000000000
-    },  
-    ethereum:{
-      // url:"https://ethereum.blockpi.network/v1/rpc/public",
-      // url:"https://ethereum.blockpi.network/v1/rpc/08477cf97ef1a6e042db5304ee957d2598d739b5",
-      url:"https://eth-mainnet.g.alchemy.com/v2/bCy-dNQogPRmiiArmblIlEn8ViU_ns7D",
-      accounts: [`${process.env.PK}`],
-      gasPrice:14000000000,
-      timeout: 1000000
-    },
-    sepolia:{
-      url:"https://ethereum-sepolia.blockpi.network/v1/rpc/public",
-      accounts: [`${process.env.PK}`],
-      gasPrice:100000000  
-    },
-    ethFork:{
-      url:"http:/192.168.31.228:8645",
-      accounts:[`${process.env.PK}`],
-      gasPrice:45000000000,
-      timeout: 1000000
     }
-
   },
-  etherscan:{
+  etherscan: {
     //bsc
     // apiKey: "I5ZF72517II2FRPQRRHMWGN7PHCFGD3NNF"
     //eth
@@ -93,5 +38,12 @@ module.exports = {
     // apiKey: "92575c4a-2830-4a7c-a40f-d7c6ed460934",
     //arbitrum
     //apiKey: "65PSNHIK3HDV9K88DNZQAM9GN8ZVCT288B"
-  }
+  },
+  typechain: {
+    outDir: './dist',
+    target: 'ethers-v5',
+    alwaysGenerateOverloads: false, // should overloads with full signatures like deposit(uint256) be generated always, even if there are no overloads?
+    externalArtifacts: [], // optional array of glob patterns with external artifacts to process (for example external libs from node_modules)
+    dontOverrideCompile: false
+  },
 };
